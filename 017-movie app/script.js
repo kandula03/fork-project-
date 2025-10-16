23887c54c36be8aa4785db968844c2e64bc11f5d
kandula03 <reddaiah0352@gmail.com>
2025-10-16 15:20:30 +0100

    orange

--- 017-movie app/script.js
+++ 017-movie app/script.js
@@ -13,6 +13,7 @@ const getClassByRate = (vote) => {
   if (vote >= 7.5) return "green";
   else if (vote >= 7) return "orange";
   else return "red";
+  \\blue pink
 };
 
 const showMovies = (movies) => {
